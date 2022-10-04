119f875e68f34f2677e3a81cd9c8e8c5a8ad163e
muratistek <messi897@gmail.com>
2022-10-03 20:22:29 -0400

    GitHub actions script updated

---
+++ build/precache-manifest.2b01ca14a2aadd7348bab2758178a6b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e8f4f5444835578ab96",
+    "url": "/muratistek/COMP3104/static/css/main.50ced0b0.chunk.css"
+  },
+  {
+    "revision": "7e8f4f5444835578ab96",
+    "url": "/muratistek/COMP3104/static/js/main.7e8f4f54.chunk.js"
+  },
+  {
+    "revision": "832d1c877218a4ebc70b",
+    "url": "/muratistek/COMP3104/static/js/runtime~main.832d1c87.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/muratistek/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "18c3dafacb6c6994f7e87e47b3fb704c",
+    "url": "/muratistek/COMP3104/index.html"
+  }
+];
